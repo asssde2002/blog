cb0405995d4fb19764bf28da557fc6f47bf9b6d8
Arthur Hung <asssde2002@gmail.com>
2024-07-06 22:22:05 +0800

    Adjust the weight

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -14,13 +14,13 @@ function HomePage() {
 
   return (
     <div className="bg-zinc-300 h-screen w-screen flex flex-col">
-      <div className="bg-blue-300 h-1/8">
+      <div className="bg-blue-300 h-[12.5%]">
         Arthur Universe
         <div>test</div>
       </div>
 
-      <div className="bg-red-900 h-20 w-2/3"></div>
-      <div className="bg-yellow-900 h-20 w-2/3"></div>
+      <div className="bg-red-900 h-[12.5%] w-2/3"></div>
+      <div className="bg-yellow-900 h-[75%] w-2/3"></div>
     </div>
   );
 }
