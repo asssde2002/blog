db45dee1b2aa845173026d862de164f5c3291dd0
Arthur Hung <asssde2002@gmail.com>
2025-01-10 21:25:54 +0800

    Update

--- frontend/MY_README.md
+++ frontend/MY_README.md
@@ -1,4 +1,10 @@
+# Create react app
+`
+npx create-react-app frontend
+`
+
 # react package
 `
+npm i web-vitals
 npm i react-router-dom
 `
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,25 +1,47 @@
 import React, { useEffect, useState } from "react";
 import config from "../config";
-import { Container, Grid, Card, CardContent, Typography } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  List,
+} from "@mui/material";
 
 function HomePage() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch(`${config.API_BASE_URL}/posts`)
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
-      .catch((error) => console.error("Error fetching posts:", error));
-  }, []);
-
   return (
     <div className="bg-zinc-300 h-screen w-screen flex flex-col">
       <div className="bg-blue-300 h-[12.5%]">
         Arthur Universe
         <div>test</div>
       </div>
-
-      <div className="bg-red-900 h-[12.5%] w-2/3"></div>
+      <div className="bg-red-900 h-[12.5%] w-2/3">
+        <span style={{ textAlign: "center" }}>QUALIFICATIONS PROFILE</span>
+        <ul>
+          <li>
+            * 3.5 years of experience as a backend engineer, contributing to the
+            development of several high-quality products
+          </li>
+          <li>
+            * Strong problem-solving skills and a collaborative team player
+          </li>
+          <li>
+            * Proficient in programming and algorithm design, with expertise in
+            Python and Django
+          </li>
+          <li>* Eager to continue learning and advancing skills</li>
+          <li>
+            * Demonstrated leadership ability, having successfully led a team of
+            5 backend engineers
+          </li>
+          <li>
+            * Extensive experience in managing and maintaining server
+            infrastructure, including provisioning, configuration, and
+            deployment
+          </li>
+        </ul>
+      </div>
       <div className="bg-yellow-900 h-[75%] w-2/3"></div>
     </div>
   );
