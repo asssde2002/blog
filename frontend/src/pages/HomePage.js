22185065d1a67f9b0a50ed43cf03db8fdad5fa7c
Arthur Hung <asssde2002@gmail.com>
2025-01-10 22:09:00 +0800

    Construct HomePage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,48 +1,200 @@
 import React, { useEffect, useState } from "react";
 import config from "../config";
-import {
-  Container,
-  Grid,
-  Card,
-  CardContent,
-  Typography,
-  List,
-} from "@mui/material";
+
+function QualificaitonProfile() {
+  return (
+    <div>
+      <ul>
+        <h3 style={{ background: "yellow" }}>QUALIFICATIONS PROFILE</h3>
+        <li>
+          3.5 years of experience as a backend engineer, contributing to the
+          development of several high-quality products
+        </li>
+        <li>Strong problem-solving skills and a collaborative team player</li>
+        <li>
+          Proficient in programming and algorithm design, with expertise in
+          Python and Django
+        </li>
+        <li>Eager to continue learning and advancing skills</li>
+        <li>
+          Demonstrated leadership ability, having successfully led a team of 5
+          backend engineers
+        </li>
+        <li>
+          Extensive experience in managing and maintaining server
+          infrastructure, including provisioning, configuration, and deployment
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Education() {
+  return (
+    <div>
+      <ul>
+        <h3 style={{ background: "yellow" }}>EDUCATION</h3>
+        <li>
+          M.S. in Computer Science & Information Engineering, National Taiwan
+          University
+          <span> | Taiwan</span>
+          <span> | Sep. 2018 - Aug. 2020</span>
+          <ul>
+            <li>Advisor: Chung-Wei Lin</li>
+            <li>
+              Master's Thesis Topic: Robust Scheduling for Intelligent
+              Intersection Management Considering Denial-of-Service Attack
+            </li>
+          </ul>
+        </li>
+        <li>
+          B.S. in Engineering Science, National Cheng Kung University
+          <span> | Taiwan</span>
+          <span> | Sep. 2014 - Jun. 2018</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function WorkExperience() {
+  return (
+    <ul>
+      <h3 style={{ background: "yellow" }}>WORK EXPERIENCE</h3>
+      <li>
+        <strong>
+          beBitTECH
+          <span> | Backend Manager</span>
+          <span> | Jan. 2022 - June. 2024</span>
+        </strong>
+        <ul>
+          <li>
+            Migrated data from legacy PostgreSQL tables to a new partitioned
+            PostgreSQL setup, enhancing performance and scalability while
+            ensuring compatibility with Django's partitioned table filtering
+            syntax.
+          </li>
+          <li>
+            Implemented migration of time-series data from PostgreSQL to
+            TimescaleDB, resulting in reduced disk usage and improved
+            efficiency.
+          </li>
+          <li>
+            Integrated Leek service into our system to effectively monitor
+            Celery task execution progress.
+          </li>
+          <li>
+            Introduced KEDA autoscaler to optimize Kubernetes pod scaling based
+            on RabbitMQ queue length, rather than CPU usage.
+          </li>
+          <li>
+            Developed a comprehensive deployment SOP for our new environment and
+            conducted rigorous load testing to simulate the impact of concurrent
+            user loads on database and API performance.
+          </li>
+          <li>
+            Introduced a backend linter (Ruff) and formatter (Isort, Ruff) to
+            ensure codebase consistency and maintain coding standards.
+          </li>
+          <li>
+            Led a team of backend developers in the design and implementation of
+            scalable and reliable systems.
+          </li>
+          <li>
+            Managed deployment and maintenance of systems on Google Cloud
+            Platform (GCP) using Docker, Kubernetes, and Dokku, ensuring
+            seamless operations and efficient resource management.
+          </li>
+          <li>
+            Analyzed complex and ambiguous requirements, proposing multiple
+            solutions after a thorough evaluation of the pros and cons of each
+            option.
+          </li>
+          <li>
+            Provided technical leadership and mentorship to team members to
+            ensure their professional growth and development.
+          </li>
+          <li>
+            Developed and implemented best practices for software development
+            processes and ensured adherence to coding standards and practices.
+          </li>
+          <li>
+            Monitored system performance metrics and provided actionable
+            recommendations for optimization and enhancement, driving continuous
+            improvement efforts.
+          </li>
+          <li>
+            Leveraged CI/CD tools like DroneCI to automate testing and
+            deployment processes, streamlining development workflows and
+            improving productivity.
+          </li>
+          <li>
+            Evaluated and refined the pretest and interview process to ensure
+            effectiveness and comprehensiveness.
+          </li>
+        </ul>
+      </li>
+      <li>
+        <strong>
+          beBitTECH
+          <span> | Backend Engineer</span>
+          <span> | Feb. 2021 - Jan. 2022</span>
+        </strong>
+        <ul>
+          <li>
+            Collaborated with cross-functional teams, including frontend
+            developers, quality assurance engineers, and product managers, to
+            deliver high-quality software solutions.
+          </li>
+          <li>
+            Designed, developed, and maintained backend systems to support
+            business requirements.
+          </li>
+          <li>
+            Integrated Customer Data Platform (CDP) platforms,
+            email/SMS/Line/push notification providers, and Facebook Ads into
+            the system infrastructure.
+          </li>
+          <li>
+            Generated dummy data for system-wide marketing purposes,
+            contributing to business growth strategies.
+          </li>
+          <li>
+            Implemented alerting mechanisms to monitor task execution status,
+            ensuring system reliability and stability.
+          </li>
+          <li>
+            Enhanced system performance by implementing efficient caching
+            mechanisms.
+          </li>
+          <li>
+            Refactored legacy code to improve performance and maintainability,
+            leveraging design patterns and coding standards.
+          </li>
+          <li>
+            Introduced comprehensive unit and integration tests to ensure code
+            quality and robustness.
+          </li>
+          <li>
+            Participated in code reviews, improving team productivity and code
+            quality.
+          </li>
+        </ul>
+      </li>
+    </ul>
+  );
+}
 
 function HomePage() {
   return (
     <div className="bg-zinc-300 h-screen w-screen flex flex-col">
       <div className="bg-blue-300 h-[12.5%]">
-        Arthur Universe
-        <div>test</div>
+        <h1>Arthur Universe</h1>
       </div>
-      <div className="bg-red-900 h-[12.5%] w-2/3">
-        <span style={{ textAlign: "center" }}>QUALIFICATIONS PROFILE</span>
-        <ul>
-          <li>
-            * 3.5 years of experience as a backend engineer, contributing to the
-            development of several high-quality products
-          </li>
-          <li>
-            * Strong problem-solving skills and a collaborative team player
-          </li>
-          <li>
-            * Proficient in programming and algorithm design, with expertise in
-            Python and Django
-          </li>
-          <li>* Eager to continue learning and advancing skills</li>
-          <li>
-            * Demonstrated leadership ability, having successfully led a team of
-            5 backend engineers
-          </li>
-          <li>
-            * Extensive experience in managing and maintaining server
-            infrastructure, including provisioning, configuration, and
-            deployment
-          </li>
-        </ul>
-      </div>
-      <div className="bg-yellow-900 h-[75%] w-2/3"></div>
+      <QualificaitonProfile />
+      <Education />
+      <WorkExperience />
+      <div></div>
     </div>
   );
 }
--- frontend/src/pages/PostPage.js
+++ frontend/src/pages/PostPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import config from "../config";
 import { Link } from "react-router-dom";
 
-import { Container, Grid, Card, CardContent, Typography } from "@mui/material";
+// import { Container, Grid, Card, CardContent, Typography } from "@mui/material";
 
 function PostPage() {
   const [posts, setPosts] = useState([]);
@@ -15,26 +15,27 @@ function PostPage() {
   }, []);
 
   return (
-    <Container>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Posts
-      </Typography>
-      <Grid container spacing={4}>
-        {posts.map((post) => (
-          <Grid item key={post.id} xs={12} sm={6} md={7}>
-            <Card>
-              <CardContent>
-                <Typography variant="h5" component="h2">
-                  <Link to={`/posts/${post.id}`}>{post.title}</Link>
-                </Typography>
-                <Typography>{post.content.substring(0, 100)}...</Typography>
-              </CardContent>
-              <CardContent></CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <h1>PostPage</h1>
+    // <Container>
+    //   <Typography variant="h4" component="h1" gutterBottom>
+    //     Posts
+    //   </Typography>
+    //   <Grid container spacing={4}>
+    //     {posts.map((post) => (
+    //       <Grid item key={post.id} xs={12} sm={6} md={7}>
+    //         <Card>
+    //           <CardContent>
+    //             <Typography variant="h5" component="h2">
+    //               <Link to={`/posts/${post.id}`}>{post.title}</Link>
+    //             </Typography>
+    //             <Typography>{post.content.substring(0, 100)}...</Typography>
+    //           </CardContent>
+    //           <CardContent></CardContent>
+    //         </Card>
+    //       </Grid>
+    //     ))}
+    //   </Grid>
+    // </Container>
   );
 }
 
